b84dacdb20422af9e5369661a20479fad364ee20
JunYang <JUNYANG.23@ichat.sp.edu.sg>
2024-05-02 14:45:52 +0800

    bmi.py

--- bmi.py
+++ bmi.py
@@ -5,10 +5,13 @@ def calculate_bmi(height,weight):
     print ("BMI = " + str(bmi))
     if bmi < 18.5 :
         print ("You are Under Weight")
+        return -1
     elif 18.5 <= bmi <= 25.0:
         print ("You are Normal Weight")
+        return 0
     else: 
         print ("You are Over Weight")
+        return 1
         
 
 
