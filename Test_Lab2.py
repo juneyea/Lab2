781e3b2be429ff5a7aedeb0b2b1ac1aefa7ed795
JunYang <JUNYANG.23@ichat.sp.edu.sg>
2024-05-03 16:47:09 +0800

    Lab3 excercise

---
+++ Test_Lab2.py
@@ -0,0 +1,14 @@
+import Lab2 as test
+
+def test_find_min_max():
+    y =  [2,19,3,5,-10]
+    result = test.find_min_max(y)
+    assert result == [19.0,-10.0]
+
+def test_calc_average():
+    y = [2,4,6,8]
+    result = test.calc_average(y)
+    assert result == 5.0
+
+def test_calc_median_temperature():
+    y=0
